bb6a965df5d495f6f09e5e6252e925ac7f2ced8b
hoanglinh08122000 <67300558+hoanglinh08122000@users.noreply.github.com>
2020-07-13 09:40:56 +0700

    test github

--- resources/views/subject/view_insert_subject.blade.php
+++ resources/views/subject/view_insert_subject.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.master')
 @section('content')
-	
+
 	<div class="card"  >
 		<div class="card-header" >
-			<strong>Thêm Môn học</strong> 
+			<strong>Thêm Môn học</strong>
 		</div>
 		<div class="card-body card-block" >
 			<form action="{{ route('students.process_insert_students') }}" method="post" enctype="multipart/form-data" class="form-horizontal">
 				@csrf
-				
-			
+
+
 				<div class="row form-group">
 					<div class="col col-md-3"><label for="text-input" class=" form-control-label">Tên</label></div>
 					<div class="col-12 col-md-9"><input type="text" id="text-input" name="name" placeholder="Text" class="form-control"><small class="form-text text-muted">This is a help text</small></div>
 				</div>
-				
-			
+
+
 				<div class="row form-group">
 					<div class="col col-md-3"><label for="select" class=" form-control-label">Giáo viên</label></div>
 					<div class="col-12 col-md-9">
@@ -30,22 +30,22 @@
 				</div>
 
 
-				
-				
-				
-				
-				
+				{{-- test github destop
+
+				 --}}
+
+
 					<button type="submit" class="btn btn-primary btn-sm" >
 						<i class="fa fa-dot-circle-o"></i> Submit
 					</button>
 					<button type="reset" class="btn btn-danger btn-sm">
 						<i class="fa fa-ban"></i> Reset
 					</button>
-				
+
 			</form>
 		</div>
-		
+
 	</div>
-	
+
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,88 +3,87 @@
 
 use Illuminate\Support\Facades\Route;
 
-route::get("login","LoginController@login")->name("login");
+route::get("login", "LoginController@login")->name("login");
 
 // login students
-route::get("login_student","LoginController@login_student")->name("login_student");
-route::post("process_login_student","LoginController@process_login_student")->name("process_login_student");
+route::get("login_student", "LoginController@login_student")->name("login_student");
+route::post("process_login_student", "LoginController@process_login_student")->name("process_login_student");
 // login admin
-route::get("login_admin","LoginController@login_admin")->name("login_admin");
-route::post("process_login_admin","LoginController@process_login_admin")->name("process_login_admin");
+route::get("login_admin", "LoginController@login_admin")->name("login_admin");
+route::post("process_login_admin", "LoginController@process_login_admin")->name("process_login_admin");
 // login teacher
-route::get("login_teacher","LoginController@login_teacher")->name("login_teacher");
-route::post("process_login_teacher","LoginController@process_login_teacher")->name("process_login_teacher");
-
+route::get("login_teacher", "LoginController@login_teacher")->name("login_teacher");
+route::post("process_login_teacher", "LoginController@process_login_teacher")->name("process_login_teacher");
 
 // middleware
-route::group(['middleware' => 'CheckLogin'],function(){
-	route::get("","Controller@index")->name("index");
+route::group(['middleware' => 'CheckLogin'], function () {
+	route::get("", "Controller@index")->name("index");
 
 	// logout
-	$controller="LoginController";
-	route::group(["prefix"=> "login" , "as" => "login."],function() use ($controller){
-		route::get("logout","$controller@logout")->name("logout");
+	$controller = "LoginController";
+	route::group(["prefix" => "login", "as" => "login."], function () use ($controller) {
+		route::get("logout", "$controller@logout")->name("logout");
 	});
 
 	//hoc sinh
-	$controller="StudentsController";
-	route::group(["prefix"=> "students" , "as" => "students."],function() use ($controller){
+	$controller = "StudentsController";
+	route::group(["prefix" => "students", "as" => "students."], function () use ($controller) {
 
-		route::get("","$controller@show_students")->name("show_students");
-		route::get("view_insert_students","$controller@view_insert_students")->name("view_insert_students");
-		route::post("process_insert_students","$controller@process_insert_students")->name("process_insert_students");
-		route::get("delete/{id}","$controller@delete")->name("delete");
-		route::get("view_update_students/{id}","$controller@view_update_students")->name("view_update_students");
-		route::post("process_update_students/{id}","$controller@process_update_students")->name("process_update_students");
+		route::get("", "$controller@show_students")->name("show_students");
+		route::get("view_insert_students", "$controller@view_insert_students")->name("view_insert_students");
+		route::post("process_insert_students", "$controller@process_insert_students")->name("process_insert_students");
+		route::get("delete/{id}", "$controller@delete")->name("delete");
+		route::get("view_update_students/{id}", "$controller@view_update_students")->name("view_update_students");
+		route::post("process_update_students/{id}", "$controller@process_update_students")->name("process_update_students");
 
 	});
 
 	// giao vien
-	$controller="TeacherController";
-	route::group(["prefix"=> "teacher" , "as" => "teacher."],function() use ($controller){
+	$controller = "TeacherController";
+	route::group(["prefix" => "teacher", "as" => "teacher."], function () use ($controller) {
 
-		route::get("","$controller@show_teacher")->name("show_teacher");
-		route::get("view_insert_teacher","$controller@view_insert_teacher")->name("view_insert_teacher");
-		route::post("process_insert_teacher","$controller@process_insert_teacher")->name("process_insert_teacher");
-		route::get("delete/{id}","$controller@delete")->name("delete");
-		route::get("view_update_teacher/{id}","$controller@view_update_teacher")->name("view_update_teacher");
-		route::post("process_update_teacher/{id}","$controller@process_update_teacher")->name("process_update_teacher");
+		route::get("", "$controller@show_teacher")->name("show_teacher");
+		route::get("view_insert_teacher", "$controller@view_insert_teacher")->name("view_insert_teacher");
+		route::post("process_insert_teacher", "$controller@process_insert_teacher")->name("process_insert_teacher");
+		route::get("delete/{id}", "$controller@delete")->name("delete");
+		route::get("view_update_teacher/{id}", "$controller@view_update_teacher")->name("view_update_teacher");
+		route::post("process_update_teacher/{id}", "$controller@process_update_teacher")->name("process_update_teacher");
 
 	});
 
 	// khoa hoc
-	$controller="CourseController"; 
-	route::group(["prefix"=> "course" , "as" => "course."],function() use ($controller){
+	$controller = "CourseController";
+	route::group(["prefix" => "course", "as" => "course."], function () use ($controller) {
 
-		route::get("","$controller@show_course")->name("show_course");
-		route::get("view_insert_course","$controller@view_insert_course")->name("view_insert_course");
-		route::post("process_insert_course","$controller@process_insert_course")->name("process_insert_course");
-		route::get("delete/{id}","$controller@delete")->name("delete");
-		route::get("view_update_course/{id}","$controller@view_update_course")->name("view_update_course");
-		route::post("process_update_course/{id}","$controller@process_update_course")->name("process_update_course");
+		route::get("", "$controller@show_course")->name("show_course");
+		route::get("view_insert_course", "$controller@view_insert_course")->name("view_insert_course");
+		route::post("process_insert_course", "$controller@process_insert_course")->name("process_insert_course");
+		route::get("delete/{id}", "$controller@delete")->name("delete");
+		route::get("view_update_course/{id}", "$controller@view_update_course")->name("view_update_course");
+		route::post("process_update_course/{id}", "$controller@process_update_course")->name("process_update_course");
 
 	});
 
 	// nganh hoc
-	$controller="DisciplineController"; 
-	route::group(["prefix"=> "discipline" , "as" => "discipline."],function() use ($controller){
+	$controller = "DisciplineController";
+	route::group(["prefix" => "discipline", "as" => "discipline."], function () use ($controller) {
 
-		route::get("","$controller@show_discipline")->name("show_discipline");
-		route::get("view_insert_discipline","$controller@view_insert_discipline")->name("view_insert_discipline");
-		route::post("process_insert_discipline","$controller@process_insert_discipline")->name("process_insert_discipline");
-		route::get("delete/{id}","$controller@delete")->name("delete");
-		route::get("view_update_discipline/{id}","$controller@view_update_discipline")->name("view_update_discipline");
-		route::post("process_update_discipline/{id}","$controller@process_update_discipline")->name("process_update_discipline");
+		route::get("", "$controller@show_discipline")->name("show_discipline");
+		route::get("view_insert_discipline", "$controller@view_insert_discipline")->name("view_insert_discipline");
+		route::post("process_insert_discipline", "$controller@process_insert_discipline")->name("process_insert_discipline");
+		route::get("delete/{id}", "$controller@delete")->name("delete");
+		route::get("view_update_discipline/{id}", "$controller@view_update_discipline")->name("view_update_discipline");
+		route::post("process_update_discipline/{id}", "$controller@process_update_discipline")->name("process_update_discipline");
 
 	});
 
 	//mon hoc
-	
-	$controller="SubjectController"; 
-	route::group(["prefix"=> "subject" , "as" => "subject."],function() use ($controller){
 
-		route::get("","$controller@show_subject")->name("show_subject");
-		route::get("view_insert_subject","$controller@view_insert_subject")->name("view_insert_subject");
+	$controller = "SubjectController";
+	route::group(["prefix" => "subject", "as" => "subject."], function () use ($controller) {
+
+		route::get("", "$controller@show_subject")->name("show_subject");
+		route::get("view_insert_subject", "$controller@view_insert_subject")->name("view_insert_subject");
 		// route::post("process_insert_discipline","$controller@process_insert_discipline")->name("process_insert_discipline");
 		// route::get("delete/{id}","$controller@delete")->name("delete");
 		// route::get("view_update_discipline/{id}","$controller@view_update_discipline")->name("view_update_discipline");
@@ -92,5 +91,3 @@ route::group(['middleware' => 'CheckLogin'],function(){
 
 	});
 });
-	
-
